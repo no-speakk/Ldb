5258647ebeb1534a37229480e5683c44f0b91e9b
Mohammad Esmaeili <mohammadt8888@gmail.com>
2021-02-18 16:24:12 +0330

    some changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/admin.js', 'public/js')
+    .sass('resources/assets/sass/admin.scss', 'public/css')
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
