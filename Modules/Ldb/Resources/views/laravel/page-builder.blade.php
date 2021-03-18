b090e56d23e591636a7dac4a7664691d908d3e87
Mohammad Esmaeili <mohammadt8888@gmail.com>
2021-03-18 05:30:08 +0330

    changes

--- Modules/Ldb/Resources/views/laravel/page-builder.blade.php
+++ Modules/Ldb/Resources/views/laravel/page-builder.blade.php
@@ -6,136 +6,85 @@
     @endslot
     @slot('custom_css')
         <style>
-            #sticky-sidebar {
+            #sticky-left-sidebar {
                 /*left: -275px;*/
                 left: 0;
                 width: 280px;
+                min-height: 400px;
                 transition: all 0.2s ease;
                 z-index: 10;
             }
             #page-temp {
                 border: 4px solid #d8dbde;
-                padding: 10px 40px;
+                padding: 40px;
             }
-            .border-blue {
-                border-color: #029ef3 !important;
-                border-style: dashed !important;
+            .builder-add-element {
+                color: darkgray;
+                cursor: pointer;
+                user-select: none;
             }
-            .temp-col-dashed {
-                border: 3px solid #d5d8db;
-                padding: 10px 40px;
-                min-height: 200px;
+            .builder-add-element:hover {
+                border-color: grey;
+            }
+            .builder-element-unselected {
+                border: 4px dashed #d7dadd;
+                padding: 50px;
+            }
+            .builder-element-selected {
+                border-color: #007bff !important;
+            }
+            .builder-element:hover{
+                border: 2px solid grey;
                 border-radius: 5px;
-                margin-top: 20px;
-                margin-bottom: 20px;
-            }
-            .empty-droppable-row {
-                height: 35px;
+                cursor: pointer;
             }
         </style>
     @endslot
 
-    {{-- form builder sidebar --}}
     <section class="row">
-        <div class="p-3 bg-dark position-fixed text-left" id="sticky-sidebar">
+        {{-- sticky-left-sidebar --}}
+        <div class="p-3 bg-dark position-fixed text-left" id="sticky-left-sidebar" dir="ltr">
             <section class="row">
-                {{-- Builder Callout --}}
-                <div class="col-6 ldb-draggable" id="draggable1" data-ldb-builder-element="callout">
-                    <div class="row text-center">
-                        <div class="col-12">
-                            <img class="mt-2 mb-2 img-thumbnail" style="width: 120px;height: 120px;" src="{{ asset('img/ldb/laragon2.png') }}" alt="">
-                        </div>
-                    </div>
-                    <div class="row text-center">
-                        <div class="col-12">
-                            <h6>Callout</h6>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- Builder Section --}}
-                <div class="col-6 ldb-draggable" id="draggable2" data-ldb-builder-element="section">
-                    <div class="row text-center">
-                        <div class="col-12">
-                            <img class="mt-2 mb-2 img-thumbnail" style="width: 120px;height: 120px;" src="{{ asset('img/ldb/laragon2.png') }}" alt="">
-                        </div>
-                    </div>
-                    <div class="row text-center">
-                        <div class="col-12">
-                            <h6>Section</h6>
-                        </div>
-                    </div>
-                </div>
+                <h6 class="m-auto">Some Icon</h6>
             </section>
         </div>
 
         {{-- page-temp --}}
-        <div class="col-9 text-left ltr" style="min-height: 600px" id="page-temp">
-{{--            <div class="row empty-droppable-row"></div>--}}
-        </div>
+        <div class="col-9 text-left ltr" style="min-height: 600px" id="page-temp"></div>
     </section>
 
     @slot('custom_js')
-        {{-- sticky sidebar hide/show functionallity --}}
-        {{--<script>
-            $(document).ready(function () {
-                $('#sticky-sidebar').on({
-                    mouseenter: function () {
-                        $(this).attr("style","left:0; transition : 'opacity 1s ease-in-out';");
-                    },
-                    mouseleave: function () {
-                        $(this).attr("style","left:-275;");
-                    }
-                });
-
-            });
-        </script>--}}
-
-        {{-- Drag Drop Functionallity --}}
-        <script>
-            $(function(){
-                // Drag
-                $(".ldb-draggable").draggable({
-                    cursor: "move",
-                    cursorAt: { top: 85, left: 85 },
-                    opacity: 0.7,
-                    helper: "clone",
-                    revert: "invalid",
-                    tolerance : "pointer",
-                    // connectToSortable: "#page-temp",
-                });
-
-                $("#page-temp").sortable();
-                $("#page-temp").disableSelection();
-
-                // Drop
-                function makeDrop(target) {
-                        target.droppable({
-                        greedy: true,
-                        classes: {
-                            "ui-droppable-hover": "border-blue",  // vaghti ba drag omadim ruye drop-zone
-                        },
-                        drop: function(event, ui) {
-                            // har elementi drag konam dynamic methodesh ro seda mizanam
-                            const builder_method = ui.draggable.data('ldb-builder-element');
-                            if (builder_method) {
-                                $(this).append(LDB_BUILDER[builder_method]());
-                            }
-
-                            makeDrop($(".temp-col-dashed, .empty-droppable-row"));
-                        },
-                    });
-                }
-                makeDrop($("#page-temp"));
-                makeDrop($(".empty-droppable-row"));
-            });
-        </script>
-
-
         {{-- Builder Functionallity --}}
         <script>
             $(function(){
                 LDB_BUILDER = {
+                    include_add_element_block : () => {
+                        return `<div class="row">
+                                    <div class="col-12 builder-add-element text-center builder-element-unselected">
+                                        <h5>Add Element +</h5>
+                                    </div>
+                                </div>`;
+                    },
+                    show_elements : (elements = []) => {
+                        let asset_path = @json(asset('img/ldb/laragon2.png'));
+                        let html = `<h6 class="text-center">Add New Element</h6><hr class="bg-gray"><div class="row">`;
+                        elements.forEach(function(element) {
+                            html += `<div class="col-6 builder-element" data-ldb-builder-element="${element}">
+                                        <div class="row text-center">
+                                            <div class="col-12">
+                                                <img class="mt-2 mb-2 img-thumbnail" style="width: 120px;height: 120px;" src="${asset_path}" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="row text-center">
+                                            <div class="col-12">
+                                                <h6>${element}</h6>
+                                            </div>
+                                        </div>
+                                    </div>`;
+                        });
+                        html += `</div>`;
+                        return html;
+                    },
                     callout : (props = {}) => {
                         let is_child = props.is_child || false;
                         let title_text = props.title_text || "عنوان";
@@ -171,7 +120,7 @@
                         }
                         return html;
                     },
-                    section : (props = {}) => {
+                    row : (props = {}) => {
                         let css_class = props.css_class || "";
                         let dir = props.dir || "rtl";
 
@@ -181,6 +130,84 @@
                     },
                 }
             });
+        </script>
+
+
+        {{-- page-temp functionallity --}}
+        <script>
+            $(function (){
+                let page_temp = $("#page-temp");
+                let left_sidebar = $("#sticky-left-sidebar");
+                let selected_element;
+
+                // on page load => include add_element_+ to page-temp
+                if (page_temp.children().length <= 1) {
+                    page_temp.html(LDB_BUILDER.include_add_element_block());
+                }
+
+                // select [.builder-add-element]
+                $(page_temp).on("click", ".builder-add-element" , function (e){
+                    $(this).addClass("builder-element-selected text-primary");
+                    left_sidebar.html(LDB_BUILDER.show_elements(['row', 'callout']));
+                    selected_element = $(this);
+                });
+
+                // unselect [.builder-add-element]
+                $('.builder-add-element').outsideClick(function(event){
+                    //code that fires when user clicks outside the element
+                    //event = the click event
+                    //$(this) = the '.target-element' that is firing this function
+                    $(this).removeClass("builder-element-selected text-primary");
+                    left_sidebar.html($('<h6 class="text-center">Some Icon</h6>'));
+                    selected_element = null;
+                })
+            }, '.exclude-something');
+        </script>
+
+
+        {{-- unselect plugin --}}
+        <script>
+            (function($) {
+
+                //when the user hits the escape key, it will trigger all outsideClick functions
+                $(document).on("keyup", function (e) {
+                    if (e.which == 27) $('body').click(); //escape key
+                });
+
+                //The actual plugin
+                $.fn.outsideClick = function(callback, exclusions) {
+                    var subject = this;
+
+                    //test if exclusions have been set
+                    var hasExclusions = typeof exclusions !== 'undefined';
+
+                    //switches click event with touch event if on a touch device
+                    var ClickOrTouchEvent = "ontouchend" in document ? "touchend" : "click";
+
+                    $('body').on(ClickOrTouchEvent, function(event) {
+                        //click target does not contain subject as a parent
+                        var clickedOutside = !$(event.target).closest(subject).length;
+
+                        //click target was on one of the excluded elements
+                        var clickedExclusion = $(event.target).closest(exclusions).length;
+
+                        var testSuccessful;
+
+                        if (hasExclusions) {
+                            testSuccessful = clickedOutside && !clickedExclusion;
+                        } else {
+                            testSuccessful = clickedOutside;
+                        }
+
+                        if(testSuccessful) {
+                            callback.call(subject, event);
+                        }
+                    });
+
+                    return this;
+                };
+
+            }(jQuery));
         </script>
     @endslot
 @endcomponent
