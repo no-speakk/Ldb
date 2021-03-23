ee86522172213b949380131a3fd6f9c39abc3460
Mohammad Esmaeili <mohammadt8888@gmail.com>
2021-03-24 01:54:17 +0430

    changes

--- Modules/Ldb/Resources/views/laravel/page-builder.blade.php
+++ Modules/Ldb/Resources/views/laravel/page-builder.blade.php
@@ -5,9 +5,6 @@
         <li class="breadcrumb-item active">زیرمنو۱</li>
     @endslot
 
-    @php
-    // section css
-    @endphp
     @slot('custom_css')
         <style>
             #sticky-left-sidebar {
@@ -30,58 +27,58 @@
                 border-radius: 5px;
                 cursor: pointer;
             }
-            #page-temp {
+            #dropArea {
                 border: 4px solid #d8dbde;
                 padding: 40px;
             }
-            #page-temp .builder-add-element {
+            #dropArea .btn-add-element {
                 color: darkgray;
                 cursor: pointer;
                 user-select: none;
             }
-            #page-temp .builder-add-element:hover {
+            #dropArea .btn-add-element:hover {
                 border-color: grey;
             }
-            #page-temp .builder-element-unselected {
+            #dropArea .element-unselected {
                 border: 4px dashed #d7dadd;
                 padding: 50px;
             }
-            #page-temp .builder-element-selected {
+            #dropArea .element-selected {
                 border-color: #007bff !important;
+            }
+            #dropArea .empty-row {
+                border: 3px solid #d7dadd;
+                padding: 50px;
+                margin-top: 40px;
+                margin-bottom: 40px;
+            }
+            #dropArea .empty-row .empty-col {
+                border: 4px dashed #d7dadd;
+                padding: 50px;
             }
         </style>
     @endslot
 
     <section class="row">
-        @php
-        // section left sidebar html
-        @endphp
         <div class="p-3 bg-dark position-fixed text-left" id="sticky-left-sidebar" dir="ltr">
             <section class="row">
                 <h6 class="m-auto">Some Icon</h6>
             </section>
         </div>
-        @php
-        // section page-temp html
-        @endphp
-        <div class="col-9 text-left ltr" style="min-height: 600px" id="page-temp"></div>
+
+        <div class="col-9 text-left ltr" style="min-height: 600px" id="dropArea"></div>
     </section>
 
     @slot('custom_js')
-        @php
-        // section LDB_BUILDER js
-        @endphp
         <script>
             $(function(){
                 LDB_BUILDER = {
-                    show_add_element_btn : () => {
+                    show_btn_addElement : () => {
                         return `<div class="row">
-                                    <div class="col-12 builder-add-element text-center builder-element-unselected">
-                                        <h5>Add Element +</h5>
-                                    </div>
+                                    <div class="col-12 btn-add-element text-center element-unselected"><h5>Add Element +</h5></div>
                                 </div>`;
                     },
-                    show_elements_on_left_sidebar : (elements = []) => {
+                    show_leftSidebar_elements : (elements = []) => {
                         let asset_path = @json(asset('img/ldb/laragon2.png'));
                         let html = `<h6 class="text-center">Add New Element</h6><hr class="sidebar-divider"><div class="row">`;
                         elements.forEach(function(element) {
@@ -137,64 +134,82 @@
                         return html;
                     },
                     row : (props = {}) => {
-                        let css_class = props.css_class || "";
-                        let dir = props.dir || "rtl";
+                        // let css_class = props.css_class || "";
+                        // let dir = props.dir || "rtl";
 
-                        return `<section class="row">
-                                    <div class="col-12 temp-col-dashed ${dir === "ltr" ? "text-left" : "text-right" } ${css_class}" dir="${dir}"></div>
+                        return `<section class="row empty-row" data-ldb-builder-element="empty-row">
+                                    <div class="col-6 btn-add-element text-center element-unselected"><h5>Add Element +</h5></div>
+                                    <div class="col-6 btn-add-element text-center element-unselected"><h5>Add Element +</h5></div>
                                 </section>`;
                     },
                 }
             });
         </script>
 
-        @php
-        // section page-temp js
-        @endphp
         <script>
             $(function (){
-                let page_temp = $("#page-temp");
+                let dropArea = $("#dropArea");
                 let left_sidebar = $("#sticky-left-sidebar");
                 let selected_element;
 
-                @php
-                // section add_el_btn init
-                @endphp
-                if (page_temp.children().length <= 1) {
-                    page_temp.html(LDB_BUILDER.show_add_element_btn());
+                // vaghti hichi to safhe nabud, btn-add-element generate kon
+                if (dropArea.children().length <= 1) {
+                    dropArea.html(LDB_BUILDER.show_btn_addElement());
                 }
 
-                @php
-                // section add_el_btn select
-                @endphp
-                $(page_temp).on("click", ".builder-add-element" , function (e){
-                    $(this).addClass("builder-element-selected text-primary");
-                    left_sidebar.html(LDB_BUILDER.show_elements_on_left_sidebar(['row', 'callout']));
+                // select Add Element +
+                $(dropArea).on("click", ".btn-add-element" , function (e){
+                    // vaghti az ye selected, ye selectede dige ro click mikonim, ghablia bayad styleshon bere
+                    $(".btn-add-element").removeClass("element-selected text-primary");
+                    $(this).addClass("element-selected text-primary");
+
+                    if ($(this).parent().hasClass("empty-row")) {
+                        left_sidebar.html(LDB_BUILDER.show_leftSidebar_elements(['callout']));
+                    } else {
+                        left_sidebar.html(LDB_BUILDER.show_leftSidebar_elements(['row', 'callout']));
+                    }
                     selected_element = $(this);
                 });
 
-                @php
-                // section add_el_btn unselect
-                @endphp
-                $('.builder-add-element').outsideClick(function(event){
-                    //code that fires when user clicks outside the element
-                    //event = the click event
-                    //$(this) = the '.target-element' that is firing this function
-                    $(this).removeClass("builder-element-selected text-primary");
-                    left_sidebar.html($('<h6 class="text-center">Some Icon</h6>'));
-                    selected_element = null;
-                }, "#sticky-left-sidebar");
+                // unselect Add Element +
+                function emptyRow_unselect_listener (){
+                    $('.btn-add-element').outsideClick(function(event){
+                        //code that fires when user clicks outside the element
+                        //event = the click event
+                        //$(this) = the '.target-element' that is firing this function
+                        $(this).removeClass("element-selected text-primary");
+                        left_sidebar.html($('<h6 class="text-center">Some Icon</h6>'));
+                        selected_element = undefined;
+                    }, "#sticky-left-sidebar, .btn-add-element");
+                }
+                emptyRow_unselect_listener();
 
-                @php
-                // section left sidebar js
-                @endphp
+                // create Add Element +
                 left_sidebar.on("click", ".builder-element" , function (e){
-                    // dynamically create new element
-                    selected_element.parent().before(LDB_BUILDER[$(this).data('ldb-builder-element')]());
+                    // $(this) = leftSidebar ,,, selectedRow = dropArea selected element
+                    let parentRow = selected_element.parent();
+                    if (parentRow.hasClass("empty-row")) {
+                        let col_width = 'col-' + (12 / parentRow.children().length);
+                        let newElement_mockup = LDB_BUILDER[$(this).data('ldb-builder-element')]();
+                        let removedParent_from_newElement = newElement_mockup
+                            .replace('<section class="row">', '')
+                            .replace('</section>', '')
+                            .replace('col-12', col_width);
+                        selected_element.replaceWith(removedParent_from_newElement);
+                        // remove attr and style from parentRow after all cols filled
+                        if (parentRow.children('.btn-add-element').length === 0) {
+                            parentRow.removeClass("empty-row");
+                            parentRow.removeAttr("data-ldb-builder-element");
+                        }
+                    } else {
+                        parentRow.before(LDB_BUILDER[$(this).data('ldb-builder-element')]());
+                    }
+                    // attach fucking listener to new elements
+                    emptyRow_unselect_listener();
                     // select new element
-                    selected_element = selected_element.parent().prev();
+                    selected_element = parentRow.prev();
                     // click on new element
-                    selected_element.click();
+                    $('body').click();
                 });
             });
         </script>
